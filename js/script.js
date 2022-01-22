5bc61d4ba713b19a2b165f560445af3d74f97e65
DanilSProjects <danielsyrov9@gmail.com>
2022-01-22 22:45:45 +0800

    Buttons correspond to display and '=' actually runs the operate function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+// DOM selection of display, buttons
+let displayText = "";
+let currentOperation = [];
+let isTheAnswer = false;
+let display = document.querySelector("#display");
+let calculatorButtons = document.querySelectorAll(".calculator-button");
+calculatorButtons.forEach((button) => {
+    button.addEventListener('click', updateDisplay)
+})
+
+// Function to update display + operate if '=' is pressed
+function updateDisplay(e) {
+    let buttonEntered = e.target.textContent;
+
+    if (buttonEntered === "=") {
+        currentOperation.push(Number(displayText));
+        console.log(currentOperation);
+        displayText = operate(currentOperation[0], currentOperation[2], currentOperation[1]);
+        display.textContent = displayText;
+        currentOperation = [];
+        isTheAnswer = true;
+    } else if (/[+\-*\/]/.test(buttonEntered)) {
+        currentOperation.push(Number(displayText));
+        displayText = buttonEntered;
+        display.textContent = displayText;
+        currentOperation.push(displayText);
+        console.log(currentOperation);
+        displayText = "";
+    } else {
+        if (isTheAnswer === true) {
+            displayText = "";
+        }
+        isTheAnswer = false;
+        displayText += buttonEntered;
+        display.textContent = displayText;
+    }
+
+    
+}
+
 // Main operate function -> takes in operator and 2 numbers
 function operate(first, second, operator) {
     switch (operator) {
